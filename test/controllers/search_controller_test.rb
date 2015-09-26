46f3d5853763e22f6cc7757dfa2c9e9a7fb0dde1
support@konagayoshi.com <konagayoshi@hotmail.com>
2015-09-26 04:20:27 +0000

    add schema.rb and test

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+#  test "the false" do
+#    assert false
+#  end
 end
